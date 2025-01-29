5facf537806bfc9067eabacdcf5b407d0caabfe0
jgierlach <jegierlach@gmail.com>
2025-01-28 21:47:12 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<div
+	class="bg-gradient-to-b from-gray-50 to-gray-100 py-16 dark:from-gray-900 dark:to-gray-800"
+	id="about"
+>
+	<div class="container mx-auto flex flex-col items-center space-y-12 px-6">
+		<div class="space-y-4 text-left">
+			<p class="mx-auto max-w-3xl text-lg text-gray-600 sm:text-xl dark:text-gray-400">
+				Hello 👋, my name is Jan-Edward. I am a fullstack developer and business owner. Over the
+				past 3 years, I’ve generated
+				<span class="font-semibold text-gray-800 dark:text-white">$2.35 million</span> in revenue by
+				creating and scaling digital products and services.
+			</p>
+		</div>
+
+		<!-- Image with Caption -->
+		<div class="flex flex-col items-center space-y-4">
+			<img
+				src="/stripe-revenue-screenshot-half.png"
+				alt="Stripe revenue screenshot"
+				class="max-w-full rounded-lg object-cover shadow-lg sm:max-w-lg lg:max-w-2xl"
+			/>
+			<p class="text-sm text-gray-500 dark:text-gray-400">
+				Stripe revenue screenshot (last 3 years)
+			</p>
+		</div>
+
+		<div class="space-y-4 text-left">
+			<p class="mx-auto max-w-3xl text-left text-lg text-gray-600 sm:text-xl dark:text-gray-400">
+				And in just under 3 years <span class="font-semibold text-gray-800 dark:text-white"
+					>$3.88</span
+				> million selling on Amazon.
+			</p>
+		</div>
+
+		<!-- <div class="max-w-3xl space-y-4 text-left sm:text-left">
+			<p class="text-l text-left text-gray-600 sm:text-xl dark:text-gray-400">
+				And in just under 3 years <span class="font-semibold text-gray-800 dark:text-white"
+					>$3.88</span
+				> million selling on Amazon.
+			</p>
+		</div> -->
+
+		<!-- Image with Caption -->
+		<div class="flex flex-col items-center space-y-4">
+			<img
+				src="/amazon-revenue-screenshot.png"
+				alt="Stripe revenue screenshot"
+				class="max-w-full rounded-lg object-cover shadow-lg sm:max-w-lg lg:max-w-2xl"
+			/>
+			<p class="text-sm text-gray-500 dark:text-gray-400">Amazon revenue 1/31/2022 - 12/31/2024</p>
+		</div>
+	</div>
+</div>
